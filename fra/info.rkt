ed08330fb075100f2899d0685ebae6fd1c9963e1
dented42 <matias@fortlogic.net>
2016-01-06 11:16:01 -0700

    corrected scribblings syntax

--- fra/info.rkt
+++ fra/info.rkt
@@ -7,6 +7,6 @@
   (list "4.x"))
 (define blurb
   (list "A purely functional implementation of a relational algebra + database"))
-(define scribblings '(("fra.scrbl" ("Database"))))
+(define scribblings '(("fra.scrbl" ())))
 (define primary-file "main.rkt")
 (define categories '(datastructures))
